1ac33e8734289ee4fb1004b0499247ee0489228c
a01653 <jesus@jqbit.com>
2026-04-16 11:55:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/mastil_escalas/",
+  base: "/mastil_prueba/",
   plugins: [react(), tailwindcss()],
 });
